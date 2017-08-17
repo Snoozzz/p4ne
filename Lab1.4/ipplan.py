1a38f8b0702793ad943e67f46dfeee3e8a101ea5
Snoozzz <staedtler@list.ru>
2017-08-17 16:55:04 +0300

    Hello

--- Lab1.4/ipplan.py
+++ Lab1.4/ipplan.py
@@ -1,7 +1,6 @@
 from ipaddress import IPv4Network
 import random
 
-#random.randint(0,100)
 list_of_network=[]
 
 na = random.randint(0x0B000000, 0xDF000000)
@@ -14,17 +13,15 @@ class IPv4RandomNetwork(IPv4Network):
         IPv4Network.__init__(self, (na, m), strict=False)
         #self.subnets = na
     def key_value(self):
-        int(self.network_address._ip)
-        int(self.netmask._ip)
-        return(self)
+        a = int(self.network_address._ip)
+        b = int(self.netmask._ip)
+        return a+b*2**32
 
-def c():
-
+def nsort(x):
+    return(x.key_value())
 
 
 for i in range(0, 50):
     list_of_network.append(IPv4RandomNetwork())
-
-  #  sorted(list_of_network, key=)
-
-print(list_of_network)
+l = sorted(list_of_network, key=nsort)
+print(l)
