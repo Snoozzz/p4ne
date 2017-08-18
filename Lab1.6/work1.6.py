46baa8e34fe78234acbf801ebdedc8765963b3ca
Snoozzz <staedtler@list.ru>
2017-08-18 18:03:40 +0300

    Hello

--- Lab1.6/work1.6.py
+++ Lab1.6/work1.6.py
@@ -1,16 +1,36 @@
 import glob
-import os
+import re
 
-#os.PathLike=('C:\\Users\\A.Meshkov\\Seafile\\p4ne_training\\config_files\\*.txt')
+
+
+#int, host, unclassified
+
 list_n=[]
 rlist = glob.glob(('C:\\Users\\A.Meshkov\\Seafile\\p4ne_training\\config_files\\*.txt'))
+
+def class_ip(x):
+
+    a = re.match("^ ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", x)
+    if a:
+        return ("IP", a.group(1), a.group(2))
+
+    a = re.match("^interface (.+)", x)
+    if a:
+        return ("INT", a.group(1))
+
+    a = re.match("^hostname (.+)", x)
+    if a:
+        return ("HOST", a.group(1))
+
+    return ("UNCLASSIFIED",)
 
 for name in rlist:
     with open(name) as f:
         str_list = list(f)
         for str in str_list:
-            if str.find('ip address ')>0:
-                list_n.append(str.replace('ip address ', '').strip())
+            p = class_ip(str)
+            if p[0] !="UNCLASSIFIED":
+                print(p)
 
 
-print(sorted(list(set(list_n))))
+
