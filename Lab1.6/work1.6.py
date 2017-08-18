2940bcfe5f6737417bb58a063d64e12501113032
Snoozzz <staedtler@list.ru>
2017-08-18 17:05:18 +0300

    Hello

---
+++ Lab1.6/work1.6.py
@@ -0,0 +1,16 @@
+import glob
+import os
+
+#os.PathLike=('C:\\Users\\A.Meshkov\\Seafile\\p4ne_training\\config_files\\*.txt')
+list_n=[]
+rlist = glob.glob(('C:\\Users\\A.Meshkov\\Seafile\\p4ne_training\\config_files\\*.txt'))
+
+for name in rlist:
+    with open(name) as f:
+        str_list = list(f)
+        for str in str_list:
+            if str.find('ip address ')>0:
+                list_n.append(str.replace('ip address ', '').strip())
+
+
+print(sorted(list(set(list_n))))
