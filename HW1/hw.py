ab4679b83b54cb8f215e1b4cc3de7ff9fdd0dc53
Snoozzz <staedtler@list.ru>
2017-08-29 12:57:34 +0300

    Hello

--- HW1/hw.py
+++ HW1/hw.py
@@ -23,11 +23,47 @@ def class_ip(x):
         return (ipaddress.IPv4Interface(a.group(1)), 10)
     return None
 
-for name in rlist:
-    with open(name) as f:
-        str_list = list(f)
-        for str in str_list:
-            p = class_ip(str)
-            print(p)
+# for name in rlist:
+#     with open(name) as f:
+#         str_list = list(f)
+#         for str in str_list:
+#             p = class_ip(str)
+#             print(p)
+
+    if __name__ == '__main__':
+
+        set_of_ip = set()
+
+        for current_file_name in glob.glob("/Users/mk/Seafile/p4ne_training/config_files/*.txt"):
+            with open(current_file_name) as f:
+                for current_line in f:
+                    addr = class_ip(current_line)
+                    if addr:
+                        set_of_ip.add(addr)
+
+        addr_plan = {}
+        hosts = []
+
+        # Fill addr_plan with subnets
+        for ip_addr in set_of_ip:
+            if ip_addr[0].network.prefixlen != 32:  # Subnets
+                subnet_key = str(ip_addr[0].network)
+                if subnet_key not in addr_plan.keys():
+                    addr_plan[subnet_key] = [(str(ip_addr[0].ip), ip_addr[1])]
+                else:
+                    addr_plan[subnet_key].append((str(ip_addr[0].ip), ip_addr[1]))
+            else:  # Hosts
+                hosts.append((ip_addr[0].ip, ip_addr[1]))
+
+        # Add gateways to addr_plan
+        for subnet_key in addr_plan.keys():
+            for gw in hosts:
+                if gw[0] in ipaddress.IPv4Network(subnet_key):
+                    addr_plan[subnet_key].append((str(gw[0]), gw[1]))
+
+print(addr_plan)
+
+
+
 
 #print(sorted(list(set(list_n))))
